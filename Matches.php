aed85088d1776fb5da3440a7a3f36cd7e70f93fd
MarcBertu <marcbertu@gmail.com>
2023-06-16 15:12:07 +0200

    Mise à jour des enums de la bdd

--- Matches.php
+++ Matches.php
@@ -1,5 +1,33 @@
 <?php
 enum Matches : string
 {
+
     case isin = '/<a\s+class="c-faceplate__company-link"\s+href="([^"]+)"\s+title="([^"]+)"/';
+
+    /* TABLE ENTREPRISE */
+    case label = "label";
+    case codeISIN = "codeISIN";
+    case codeEnt = "codeEnt";
+    case pays = "pays";
+
+    /* TABLE COURS */
+    case date_heure = "date_heure";
+    case ouverture = "ouverture_cours";
+    case cloture = "cloture_veille";
+    case haut = "cours_haut";
+    case bas = "cours_bas";
+    case volume ="volume";
+    case variation = "variation";
+    case valorisation = "valorisation"; // Facultatif
+    case risque_esg = "risque_esg"; // Facultatif
+    case date_derniere_dividende = "date_last_dividende"; // Facultatif
+
+    /* TABLE MESSAGE */
+
+    case title = "titre";
+    case content = "content";
+    case sender = "emetteur";
+    case date = "date";
+    case idMessageTo = "id_message_to"; // id du message auquel il répond (-1 par défaut)
+
 }
