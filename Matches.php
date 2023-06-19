bdbb9392a01f39374a214fc040f809321151013f
Maël Roy <dinocraftdidi@gmail.com>
2023-06-19 14:42:36 +0200

    add regex

--- Matches.php
+++ Matches.php
@@ -1,33 +1,29 @@
 <?php
 enum Matches : string
 {
-    case isin = '/<a\s+class="c-faceplate__company-link"\s+href="([^"]+)"/';
-    case title = '<a\s+class="c-faceplate__company-link"\s+[^^]{0,100}\s+title="([^"]+)"';
+    case codeUrl = '/<a\s+class="c-faceplate__company-link"\s+href="([^"]+)"/';
+    case title = '/<a\s+class="c-faceplate__company-link"\s+[^^]{0,100}\s+title="([^"]+)"/';
 
     /* TABLE ENTREPRISE */
-    case label = "label";
-    case codeISIN = "codeISIN";
-    case codeEnt = "codeEnt";
-    case pays = "pays";
+    case codeIsin = '/<h2\s+class="c-faceplate__isin">([A-Z0-9]+)/';
 
-    /* TABLE COURS */
-    case date_heure = "date_heure";
-    case ouverture = "ouverture_cours";
-    case cloture = "cloture_veille";
-    case haut = "cours_haut";
-    case bas = "cours_bas";
-    case volume ="volume";
-    case variation = "variation";
-    case valorisation = "valorisation"; // Facultatif
-    case risque_esg = "risque_esg"; // Facultatif
-    case date_derniere_dividende = "date_last_dividende"; // Facultatif
+    case codeEnt = '/<h2\s+class="c-faceplate__isin">[A-Z0-9]+\s+([A-Z]+)/';
 
-    /* TABLE MESSAGE */
+    case ouverture = '/<span\s+class="c-instrument c-instrument--open" data-ist-open>([\d+ ?]+[.]{0,1}[\d]+)/';
+    case cloture = '/<span\s+class="c-instrument c-instrument--previousclose" data-ist-previousclose>([\d+ ?]+[.]{0,1}[\d]+)/';
+    case haut = '/<span\s+class="c-instrument c-instrument--high" data-ist-high>([\d+ ?]+[.]{0,1}[\d]+)/';
+    case bas = '/<span\s+class="c-instrument c-instrument--low" data-ist-low>([\d+ ?]+[.]{0,1}[\d]+)/';
+    case volume = '/<span\s+class="c-instrument c-instrument--totalvolume" data-ist-totalvolume>([\d+ ?]+[.]{0,1}[\d]+)/';
+    case variation = '/<span\s+class="c-instrument c-instrument--variation" data-ist-variation>([\d+ ?]+[.]{0,1}[\d]+)/';
 
-    case title = "titre";
-    case content = "content";
-    case sender = "emetteur";
-    case date = "date";
-    case idMessageTo = "id_message_to"; // id du message auquel il répond (-1 par défaut)
+    
+//
+//    /* TABLE MESSAGE */
+//
+//    case forumTitle = "titre";
+//    case forumContent = "content";
+//    case orumSender = "emetteur";
+//    case orumDate = "date";
+//    case orumIdMessageTo = "id_message_to"; // id du message auquel il répond (-1 par défaut)
 
 }
--- core/fetch.function.php
+++ core/fetch.function.php
@@ -1,5 +1,32 @@
 <?php
-function fetch_with_code($code): bool|string
+
+/**
+ * @param string $code
+ * @return string
+ */
+function fetch_with_code(string $code): string
 {
-    return file_get_contents(`https://www.boursorama.com/cours/$code/`);
+    return file_get_contents('https://www.boursorama.com/cours/'.$code.'/');
+}
+
+function curl_with_code(string $code): string
+{
+    $url = 'https://www.boursorama.com/cours/' . $code . '/';
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+    $result = curl_exec($curl);
+
+    if ($result === false) {
+        $error = curl_error($curl);
+
+        var_dump($error);
+        // Gérer l'erreur de cURL
+        return $error;
+    } else {
+        // Traiter le contenu HTML récupéré
+        return $result;
+    }
 }
